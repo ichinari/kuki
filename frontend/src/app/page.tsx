926cdb8999435fff8da58f4d9f5f0a0117812e05
kzoo <>
2026-05-21 22:40:49 +0900

    add: サーバー側での認証チェックを追加し、未ログイン時にログインページへリダイレクト

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,30 @@
+import { redirect } from "next/navigation";
 import SignoutButton from "@/components/SignoutButton";
+import { createClient } from "@/utils/supabase/server";
+import { ROUTES } from "@/routes/route";
 
-export default function Home() {
+export default async function Home() {
+  // DOM レンダリング前にサーバー側で認証チェック → 未ログインならログインページへ
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect(ROUTES.LOGIN.path);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
-      <h1 className="text-2xl font-bold">KUKIへようこそ</h1>
+    <main className="flex flex-col items-center justify-center gap-4">
+      {/* SP */}
+      <div className="md:hidden">
+        <h1 className="text-2xl font-bold">SP KUKIへようこそ</h1>
+      </div>
+
+      {/* PC */}
+      <div className="hidden md:block">
+        <h1 className="text-2xl font-bold">PC KUKIへようこそ</h1>
+      </div>
       <SignoutButton />
     </main>
   );
